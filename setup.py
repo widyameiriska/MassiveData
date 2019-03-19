cd4972c23db63b2037fd1eef43d1196c5faa327a
Widya Meiriska <widya.meiriska@gmail.com>
2019-03-19 11:37:39 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='task 1 of massive data processing',
+    author='widya',
+    license='MIT',
+)
